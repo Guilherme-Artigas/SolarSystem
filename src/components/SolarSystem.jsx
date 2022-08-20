ce85b352c6c8a271e5da685f1b8fe77593fb1c2a
Guilherme Artigas <guilherme.artigas92@gmail.com>
2022-08-20 15:35:47 -0300

    Estilização 1.0

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import Title from './Title';
 import planets from '../data/planets';
 import PlanetCard from './PlanetCard';
+import '../App.css';
 
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system">
+      <div data-testid="solar-system" className="solar-system">
         <Title headline="Planetas" />
         {planets.map((e) => (
-          <PlanetCard planetName={ e.name } planetImage={ e.image } key={ e.name } />
+          <PlanetCard
+            planetName={ e.name }
+            planetImage={ e.image }
+            key={ e.name }
+          />
         ))}
       </div>
     );
